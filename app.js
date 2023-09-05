6cc7664198129914ed6fb2ad408b24e8fed0aa65
hi2suresh <hi2suresh@gmail.com>
2023-09-06 05:11:15 +1000

    removed unused exports

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
 
-const button = document.getElementById('btn')
-const span = document.querySelector('.color')
+const button = document.getElementById('btn');
+const span = document.querySelector('.color');
 function getRandomNumber() {
-  return Math.floor(Math.random() * colors.length)
+  return Math.floor(Math.random() * colors.length);
 }
 
-button.addEventListener('click', function(){
-  const randomNumber = getRandomNumber()
-  document.body.style.backgroundColor = colors[randomNumber]
-  span.textContent = colors[randomNumber]
-})
-
-export { span, getRandomNumber}
-
+button.addEventListener('click', function () {
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  span.textContent = colors[randomNumber];
+});
